65908565d93f86334b2e6acd361a922cc6e46d88
Arunkumar Parthasarathy <arunkumar.ps1988@gmail.com>
2023-04-28 17:19:11 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        PrintState(false);
+        PrintState(true);
+        PrintState(null);
+    }
+
+    //Example 1
+    public static void PrintState(bool? state)
+    {
+        var message = state switch
+        {
+            true => "State is true",
+            false => "State is false",
+            _ => "State is indeterminate",
+        };
+
+        Console.WriteLine(message);
+    }
+
+    // Example 2
+    // Compiler show warning, we used true, false and null in main method, 
+    // but in PrintState function not include the false & default conditions.
+
+    // public static void PrintState(bool? state)
+    // {
+    //     var message = state switch
+    //     {
+    //         true => "State is true"
+    //     };
+
+    //     Console.WriteLine(message);
+    // }
+
+    // Example 3
+    // public static void PrintState(bool? state)
+    // {
+    //     Console.WriteLine(state switch
+    //     {
+    //         true => "State is true",
+    //         false => "State is false",
+    //         _ => "State is indeterminate",        
+    //     });
+    // }
+}
